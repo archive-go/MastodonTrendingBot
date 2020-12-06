004d3be355c2bab0a92be735eec388a0791443df
林水溶 <shuirong1997@icloud.com>
2020-12-06 22:25:46 +0800

    feat: 增加pm2对数据库文件的忽略;

--- build/pm2.config.js
+++ build/pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
             script: 'chmod +rwx ./TrendingBot && ./TrendingBot',
             watch: '.',
             instances: 1,
-            ignore_watch: ['logs'],
+            ignore_watch: ['logs', 'path'],
             log_date_format: 'YYYY-MM-DD HH:mm:ss', //设置日志的时间格式
             log_type: 'json', //输出的日志信息为json格式
             error_file: './logs/error.log', //设置标准错误流日志要写入到哪个文件,代码错误可在此文件查找
